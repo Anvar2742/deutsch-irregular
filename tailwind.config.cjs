0329db9d670d86d6944bd9085c13edc13edc2eb7
marco12-star <99267663+Anvar2742@users.noreply.github.com>
2022-12-07 17:18:54 +0100

    font update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,9 @@ module.exports = {
             boxShadow: {
                 card: "-1px 0px 8px 2px rgba(0,0,0,0.2)",
             },
+            fontFamily: {
+                sans: ["Lato", "sans-serif"],
+            },
         },
     },
     plugins: [],

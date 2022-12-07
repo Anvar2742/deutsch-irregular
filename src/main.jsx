030ac2efd6c8f591a4f81d02946f381ffe714226
marco12-star <99267663+Anvar2742@users.noreply.github.com>
2022-12-07 17:44:59 +0100

    base update to repo name

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Router>
+        <Router basename="/deutsch-irregular/">
             <App />
         </Router>
     </React.StrictMode>
